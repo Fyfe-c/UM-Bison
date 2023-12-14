c16d58e17ca1047299d397be5211d0c479bdc340
Fyfe-c <111027432+Fyfe-c@users.noreply.github.com>
2023-12-14 16:27:34 -0600

    Update RNN2.py
    code final version

--- RNN2.py
+++ RNN2.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from imblearn.over_sampling import SMOTE
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
 
 # Data loading and preprocessing
 df = pd.read_csv('./updated_weather_data_with_states.csv')
-# df = pd.read_csv('./Data/Data/Test.csv')
 
 # Dropping non-numeric columns and columns with a large number of missing values
-# columns_to_drop = ['name', 'solarradiation', 'solarenergy', 'uvindex', 'severerisk', 'snow','sunset','sunrise','moonphase','conditions','description','icon','stations','visibility','cloudcover','sealevelpressure','winddir','windgust','snowdepth','preciptype','precipcover','precipcover','precip']
 columns_to_drop = ['name', 'solarradiation', 'solarenergy', 'uvindex', 'severerisk', 'snow']
 df = df.drop(columns=columns_to_drop, axis=1)
 
@@ -23,52 +21,47 @@ df = df.sort_values(by='datetime')
 # Filling missing values
 df.fillna(df.mean(), inplace=True)
 
-# Splitting the data using a new split date
-split_date = pd.Timestamp('2006-01-01')  # Update this date according to your dataset
-train = df[df['datetime'] < split_date]
-test = df[df['datetime'] >= split_date]
-
-# Checking the size of the datasets
-print("Training set size:", train.shape)
-print("Test set size:", test.shape)
-
 # Splitting features and labels
-X_train = train.select_dtypes(include=[np.number]).drop('result', axis=1)
-y_train = train['result']
-X_test = test.select_dtypes(include=[np.number]).drop('result', axis=1)
-y_test = test['result']
+X = df.select_dtypes(include=[np.number]).drop('result', axis=1)
+y = df['result']
 
 # Applying standardization
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_scaled = scaler.fit_transform(X)
 
 # Reshaping for RNN format
-X_train_rnn = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
-X_test_rnn = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+X_rnn = X_scaled.reshape((X_scaled.shape[0], 1, X_scaled.shape[1]))
 
 # Addressing data imbalance using SMOTE
 smote = SMOTE()
-X_train_smote, y_train_smote = smote.fit_resample(X_train_rnn.reshape(X_train_rnn.shape[0], -1), y_train)
-X_train_smote = X_train_smote.reshape((X_train_smote.shape[0], 1, X_train_smote.shape[1]))
+X_smote, y_smote = smote.fit_resample(X_rnn.reshape(X_rnn.shape[0], -1), y)
+X_smote = X_smote.reshape((X_smote.shape[0], 1, X_smote.shape[1]))
 
-# Building LSTM model
-model = Sequential()
-model.add(LSTM(50, input_shape=(X_train_smote.shape[1], X_train_smote.shape[2])))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='sigmoid'))
+# Define the LSTM model creation function
+def create_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(50, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.summary()
+# KFold cross-validation
+kfold = KFold(n_splits=10, shuffle=True)
+scores = []
 
-# Training the model
-model.fit(X_train_smote, y_train_smote, epochs=10, batch_size=32, validation_data=(X_test_rnn, y_test))
+for train, test in kfold.split(X_smote, y_smote):
+    # Create the model
+    model = create_model((X_smote.shape[1], X_smote.shape[2]))
 
-# Model evaluation
-score = model.evaluate(X_test_rnn, y_test, verbose=0)
-print('Test accuracy:', score[1])
+    # Train the model
+    model.fit(X_smote[train], y_smote[train], epochs=10, batch_size=32, verbose=0)
 
-# Display confusion matrix and classification report
-y_pred = (model.predict(X_test_rnn) > 0.5).astype("int32")
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+    # Evaluate the model
+    predictions = model.predict(X_smote[test])
+    y_pred = (predictions > 0.5).astype("int32")
+    score = accuracy_score(y_smote[test], y_pred)
+    scores.append(score)
+
+# Print cross-validation results
+print("Cross-Validation Accuracy: %.2f%% (%.2f%%)" % (np.mean(scores)*100, np.std(scores)*100))
